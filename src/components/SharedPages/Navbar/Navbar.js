ba9dc7670f33c9b7cd38bd57cf80e205ce22a2da
mizanor21 <mizanorrahman398@gmail.com>
2022-11-10 21:18:06 +0600

    Nabver code changes

--- src/components/SharedPages/Navbar/Navbar.js
+++ src/components/SharedPages/Navbar/Navbar.js
@@ -15,45 +15,108 @@ const Navbar = () => {
             })
     }
     return (
-        <div className=" bg-red-700 text-white ">
+        <div className='bg-red-700 text-white'>
             <div className="navbar container mx-auto">
-                <div className="flex-1">
-                    <Link to={'/'} className="btn btn-ghost normal-case text-3xl">HungryNaki</Link>
-                </div>
-                <div className="flex-none">
-                    {
-                        currentUser?.uid ?
-
-                            <>
-                                <div className="">
-                                    <ul className='flex text-lg font-bold mr-5'>
-                                        <Link to={''}><li className='pr-5'>My Reviews</li></Link>
-                                        <Link to={'/add-service'}><li>Add Services</li></Link>
-                                    </ul>
-                                </div>
-                                <div className="dropdown dropdown-end">
-                                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                                        <div className="w-10 rounded-full">
-                                            <img className='w-12 h-12 rounded-full border-2 ' src={currentUser?.photoURL} alt="Profile not found!" />
-                                        </div>
-                                    </label>
-                                    <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-black rounded-box w-60">
-                                        <p className='text-slate-500'>Sign in as</p>
-                                        <li className='my-3'>{currentUser?.displayName}</li>
-                                        <hr />
-                                        <li className='my-3'>{currentUser?.email}</li>
-                                        <hr />
-                                        <li onClick={handleLogOut} className='cursor-pointer my-3'>Logout</li>
-                                    </ul>
-                                </div>
-                            </>
-                            :
-                            <>
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><Link to={'/'}>Home</Link></li>
+                            <li><Link to={'/blog'}>Blog</Link></li>
+                            {
+                                currentUser?.uid ?
+                                    <>
+                                        <li><Link to={''}><li className=''>My Reviews</li></Link></li>
+                                        <li><Link to={'/add-service'}><li>Add Services</li></Link></li>
+                                    </>
+                                    :
+                                    <></>
+                            }
+                            <hr />
+                            <div className="text-center my-3">
                                 <Link to={'/signin'}>
-                                    <button className="btn glass mr-3">Signin</button>
+                                    <button className="btn btn-outline btn-primary mr-3">Signin</button>
                                 </Link>
-                            </>
-                    }
+                                <Link to={'/signup'}>
+                                    <button className="btn btn-outline btn-secondary">Signup</button>
+                                </Link>
+
+                            </div>
+                        </ul>
+                    </div>
+                    <Link to={'/'} className="flex items-center justify-center ">
+                        <Link to={'/'} className="btn btn-ghost normal-case text-3xl p-0 m-0">
+                            {/* <img className='w-24 ' src={logo} alt="logo not found" /> */}
+                            Hungry<span className='text-amber-400'>Naki</span></Link>
+
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal font-bold p-0">
+                        <li><Link to={'/'}>Home</Link></li>
+                        <li><Link to={'/blog'}>Blog</Link></li>
+                        {
+                            currentUser?.uid ?
+                                <>
+                                    <li><Link to={''}><li className=''>My Reviews</li></Link></li>
+                                    <li><Link to={'/add-service'}><li>Add Services</li></Link></li>
+                                </>
+                                :
+                                <></>
+                        }
+                    </ul>
+                </div>
+
+                <div className="navbar-end ">
+                    <div className="mr-3">
+                        <label title='Toggle Dark/Light Mode' className="swap swap-rotate">
+
+                            <input type="checkbox" />
+
+                            <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" /></svg>
+
+                            <svg className="swap-on fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
+
+                        </label>
+                    </div>
+                    <div className="signIn">
+                        <div>
+                            {
+                                currentUser?.uid ?
+
+                                    <div className="dropdown dropdown-end">
+                                        <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                                            <div className="w-10 rounded-full">
+                                                <img className='w-12 h-12 rounded-full border-2 ' src={currentUser.photoURL} alt="Profile not found!" />
+                                            </div>
+                                        </label>
+                                        <ul tabIndex={0} className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-60">
+                                            <p className='text-slate-500'>Sign in as</p>
+                                            <li className='my-3'>{currentUser?.displayName}</li>
+                                            <hr />
+                                            <li className='my-3'>{currentUser?.email}</li>
+                                            <hr />
+                                            <li onClick={handleLogOut} className='cursor-pointer my-3'>Logout</li>
+                                        </ul>
+                                    </div>
+                                    :
+                                    <>
+                                        <Link to={'/signin'}>
+                                            <button className="btn glass mr-3">Signin</button>
+                                        </Link>
+                                        {/* <Link to={'/signup'}>
+                                            <button className="btn glass mr-3">Signup</button>
+                                        </Link> */}
+                                    </>
+                            }
+                        </div>
+
+
+
+                    </div>
+
                 </div>
             </div>
         </div>
